a5be25ac6f76452ebd14e50fb96a7fd2be828794
SNOWNUMB <mamayev.stas@gmail.com>
2025-02-15 10:28:50 +0500

    Данные добавлены - 14.02.2025

--- HW_2/HomeWork/urls.py
+++ HW_2/HomeWork/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .api import StudentViewSet, CourseViewSet
-from .views import student_list, course_list, index  # Импортируем index
+from .views import student_list, course_list, index
 
 router = DefaultRouter()
 router.register(r'students', StudentViewSet)
@@ -9,7 +9,7 @@ router.register(r'courses', CourseViewSet)
 
 urlpatterns = [
     path("api/", include(router.urls)),
-    path("", index, name="index"),  # Используем index как главное представление
+    path("", index, name="index"),
     path("students/", student_list, name="students"),
     path("courses/", course_list, name="courses"),
 ]
